9766f1d989f554199d62654630c3be862b481b72
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-06-06 20:46:14 +0300

    learning about validation in django forms

--- App/forms.py
+++ App/forms.py
@@ -1,8 +1,57 @@
 from django import forms
 from .models import Candidate
+from django.core.validators import RegexValidator
 
 class CandidateForm(forms.ModelForm):
 
+    #  VALIDATIONS
+    firstname = forms.CharField(
+        label='Nom', min_length=3, max_length=50, 
+        validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ\s]*$', 
+        message="Only letters is allowd !")],
+        required=True, 
+        widget=forms.TextInput(attrs={'placeholder':'Votre nom'})
+    )
+
+    lastname = forms.CharField(
+        label='Post-nom', min_length=3, max_length=50, 
+        validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ\s]*$', 
+        message="Only letters is allowd !")],
+        widget=forms.TextInput(attrs={'placeholder':'Votre post-nom'})
+    )
+    
+    email = forms.EmailField(
+        label='Adresse mail', min_length=10, max_length=50, 
+        validators=[RegexValidator(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', 
+        message="Put a valid email address !")],
+        widget=forms.TextInput(attrs={'placeholder':'Votre Adresse mail'})
+    )
+
+    # meth 1
+    # age = forms.CharField(widget=forms.TextInput(attrs={'type':'number'}))
+    
+    # meth 1
+    
+    age = forms.CharField(
+        label='Age', min_length=2, max_length=3,
+        validators=[RegexValidator(r'^[0-9]*$', 
+        message="Only Numbers is allowd !")],
+        widget=forms.TextInput(attrs={'placeholder':'Votre Age'})
+    )
+    
+    
+    message = forms.CharField(
+        label='Talk little about you', 
+        min_length=50, max_length=1000,
+        widget=forms.Textarea(attrs={'placeholder':'Parlez brievement de vous', 'rows':7})
+    )
+    
+
+#     Name: ^[a-zA-ZÀ-ÿ\s]*$
+#     Email: ^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$
+#     Age: ^[0-9]*$
     class Meta:
         model = Candidate
         fields = "__all__"
+        # fields = ['firstname', 'lastname', 'email', 'age', 'message']
+        # exclude = ['firstname', 'lastname', 'email', 'age', 'message']
--- App/models.py
+++ App/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Candidate(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
+    age = models.CharField(max_length=3)
     email = models.EmailField(max_length=70)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
